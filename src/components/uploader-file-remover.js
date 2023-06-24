c1ffa979d2aca33fa3564dfcd3a8eeb26e4786f4
HebrinJ <ashram28@gmail.com>
2023-06-24 17:24:14 +0400

    bugfix: Исправлено удаление файлов

--- src/components/uploader-file-remover.js
+++ src/components/uploader-file-remover.js
@@ -1,15 +1,25 @@
 function handleFileListClick(evt) {
   evt.preventDefault();
-
+  
   if (evt.target.classList.contains('uploader__btn-delete')) {
     evt.target.closest('.uploader__item').remove();
   }
 }
 
-export function setFilesRemover() {
-  const filesList = document.querySelector('.uploader__files-list');
+// export function setFilesRemover() {
+//   const filesList = document.querySelector('.uploader__files-list');
+  
+//   if (filesList) {
+//     filesList.addEventListener('mousedown', handleFileListClick);
+//   }
+// }
 
-  if (filesList) {
-    filesList.addEventListener('mousedown', handleFileListClick);
+export function setFilesRemover() {
+  const filesLists = document.querySelectorAll('.uploader__files-list');
+  
+  if (filesLists.length > 0) {
+    filesLists.forEach(element => {
+      element.addEventListener('mousedown', handleFileListClick);      
+    });
   }
 }
